a662842fb77399e00308474df808d69175dbf79c
gabrielbaron01 <gabrielreisbaron@hotmail.com>
2024-06-18 16:16:52 -0300

    finalizando a dashboard

--- lib/repository/dashboard_repository.dart
+++ lib/repository/dashboard_repository.dart
@@ -2,21 +2,19 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 
 class DashboardRepository {
-  final Map<String, ImageProvider> imagens = {
-    '001': FileImage(File('')),
-    '002': FileImage(File('')),
-    '003': FileImage(File('')),
-    '004': FileImage(File('')),
-    '005': FileImage(File('')),
-    '006': FileImage(File('')),
-    '007': FileImage(File('')),
-    '008': FileImage(File('')),
-    '009': FileImage(File('')),
-    '010': FileImage(File('')),
-    '011': FileImage(File('')),
-    '012': FileImage(File('')),
-    '013': FileImage(File('')),
-    '014': FileImage(File('')),
-    // Adicione mais mapeamentos conforme necessário
-  };
+  final List<String> images = [
+    'images/bb.png',
+    'images/bmg.png',
+    'images/bradesco.png',
+    'images/c6bank.png',
+    'images/caixa.png',
+    'images/inter.png',
+    'images/itau.png',
+    'images/neon.png',
+    'images/original.png',
+    'images/pan.png',
+    'images/picpay.png',
+    'images/santander.png',
+    'images/sofisa.png',
+  ];
 }
