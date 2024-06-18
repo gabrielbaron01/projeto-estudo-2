25cecec289bd37d955a4d909ae1a2409086e635d
gabrielbaron01 <gabrielreisbaron@hotmail.com>
2024-06-18 15:17:16 -0300

    first commit

---
+++ lib/repository/dashboard_repository.dart
@@ -0,0 +1,22 @@
+import 'dart:io';
+import 'package:flutter/material.dart';
+
+class DashboardRepository {
+  final Map<String, ImageProvider> imagens = {
+    '001': FileImage(File('')),
+    '002': FileImage(File('')),
+    '003': FileImage(File('')),
+    '004': FileImage(File('')),
+    '005': FileImage(File('')),
+    '006': FileImage(File('')),
+    '007': FileImage(File('')),
+    '008': FileImage(File('')),
+    '009': FileImage(File('')),
+    '010': FileImage(File('')),
+    '011': FileImage(File('')),
+    '012': FileImage(File('')),
+    '013': FileImage(File('')),
+    '014': FileImage(File('')),
+    // Adicione mais mapeamentos conforme necessário
+  };
+}
